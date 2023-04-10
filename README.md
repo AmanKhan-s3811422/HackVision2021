0ace5edf24ea97cec4e6d9520e30701ecd568bed
Aman Khan <79898461+AmanKhan-s3811422@users.noreply.github.com>
2023-04-10 19:12:46 +1000

    Main files

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# HackVision2021
+# The Eternals
+Project Code for HackVision 2021 - Team 4. The application an online/local homework help website written using PHP, SQL, Bootstrap, HTML and CSS.
+
+* Test Account Login Details: Email = test@gmail.com; Password = test
+
+# Running the code
+1. Download XAMPP
+2. Have this project within XAMPP as in C:\xampp\htdocs\The-Eternals-\
+3. Open the Xampp Control Panel and click on 'Start' next to Apache
+4. Go to: http://localhost/The-Eternals-/
+
+# Opening & Accessing the SQLite database
+1. Download SQLite Studio
+2. Open SQLite Studio
+3. Click on 'Database' from the navigation bar
+4. From the drop-down, click 'Add a database'
+5. Under the 'File' lable or next to the file field, click on the 'folder' icon
+6. Locate database.db and select it and click 'open'
+
+# Figma Link
+https://www.figma.com/proto/71GjlkxAtCX1elhz2QARvR/EduPal?node-id=9%3A3&scaling=contain&page-id=9%3A2&starting-point-node-id=9%3A3&show-proto-sidebar=1
+
+# Authors
+* Linda Vu
+* Harshita Kumar
+* Jeremy Ng Kwik Tung
+* Aman Khan
+* Aditya Vadgama
---
+++ index.php
@@ -0,0 +1,208 @@
+<?php require_once('model/user.php'); ?>
+<?php require_once('model/post.php'); ?>
+<?php require_once('model/chat.php'); ?>
+<?php require_once('includes/functions.inc.php'); ?>
+<?php require_once('crud/postCrud.php'); ?>
+<?php require_once('crud/chatCrud.php'); ?>
+<?php
+    $currentUser = getLoggedInUser();
+    $postCrud = new PostCrud();
+    $chatCrud = new ChatCrud();
+    $allChats = [];
+    if (!empty($_SESSION[USER_SESSION_KEY])){
+        $allPosts = $postCrud->getAllPostsByEmail($currentUser->getEmail());
+        if ($currentUser->getMemberType() == "mentor") {
+            $allChats = $chatCrud->getChatsByMentor($currentUser->getEmail());
+        } else {
+            $allChats = $chatCrud->getChatsByMentee($currentUser->getEmail());
+        }
+    }
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="images/logo.png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="styles/header.css">
+    <link rel="stylesheet" href="styles/home.css">
+    <title>EduPal</title>
+  </head>
+
+  
+  <body>
+    <?php
+     require_once('includes/header.inc.php');
+    ?>
+    <article>
+
+        <div class="container-fluid">
+        <?php if (isset($currentUser)) { ?>
+            <br>
+            <!-- If logged in, show dashboard -->
+            <h1 class="welcome">Dashboard</h1>
+            <?php if ($currentUser->getMemberType() == "mentee") { ?>
+            <!-- Your posts -->
+            <br>
+            <div class="row">
+                <div class="col-sm-1"></div>
+        
+                <div class="col-sm-2">
+                    <h2 class="your-posts-label">Your Posts</h2>
+                </div>
+                
+                <div class="col-sm-1">
+                    <a href="createpost/">
+                        <button type="button" class="posts-buttons">Create</button>
+                    </a>
+                </div>
+                <div class="col-sm-6"></div>
+        
+                <div class="col-sm-1">
+                    <a href="posts/">
+                        <button type="button" class="posts-buttons see-all-button">See all</button>
+                    </a>
+                </div>
+        
+                <div class="col-sm-1"></div>
+            </div>
+        
+            <hr/>
+        
+            <p style="page-break-after: always;">&nbsp;</p>
+            <?php if (count($allPosts) > 0) { ?>
+                <table>
+                    <?php foreach ($allPosts as $post): ?>
+                        <tr onclick="window.location='post/?id=<?php echo $post->getPostId(); ?>'">
+                            <td>
+                                <b><?php echo $post->getEmail(); ?></b>
+                                <p>Subjects: <?php echo $post->getSubject(); ?></p>
+                            </td>
+                            <td>
+                                <p>Area: <?php echo $post->getAddress(); ?></p>
+                            </td>
+                            <td>
+                                <p><?php echo date('d M Y', $post->getTimestamp()); ?></p>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+            <?php } else { ?>
+                <h2 class="aligncenter">You have no posts. Create one to get help face-to-face from those around your neighbourhood!</h2>
+    
+            <?php } ?>
+            <p style="page-break-before: always;">&nbsp;</p>
+            <p style="page-break-after: always;">&nbsp;</p>
+            <p style="page-break-before: always;">&nbsp;</p>
+            <p style="page-break-after: always;">&nbsp;</p>
+            <p style="page-break-before: always;">&nbsp;</p>
+        
+            <hr/>
+            <?php } ?>
+            <!-- Your chats -->
+            <div class="row">
+                <div class="col-sm-1"></div>
+        
+                <div class="col-sm-2">
+                    <h2 class="your-posts-label">Your Chats</h2>
+                </div>
+                
+                <div class="col-sm-1">
+                    <!-- <a href="createchats/">
+                        <button type="button" class="posts-buttons">Create</button>
+                    </a> -->
+                </div>
+                
+                <div class="col-sm-6"></div>
+        
+                <div class="col-sm-1">
+                    <a href="chats/">
+                        <button type="button" class="posts-buttons see-all-button">See all</button>
+                    </a>
+                </div>
+        
+                <div class="col-sm-1"></div>
+            </div>
+        
+            <hr/>
+        
+            <p style="page-break-after: always;">&nbsp;</p>
+            <?php if (count($allChats) > 0) { ?>
+                <table>
+                    <?php foreach ($allChats as $chat): ?>
+                        <?php
+                        $postDetails = $postCrud->getPostById($chat->getPostId());
+                        ?>
+                        <tr onclick="window.location='chat/?post_id=<?php echo $chat->getPostId(); ?>&mentee_id=<?php echo $chat->getMentee(); ?>&mentor_id=<?php echo $chat->getMentor(); ?>'">
+                            <td>
+                                <?php if ($currentUser->getMemberType() == "mentor") { ?>
+                                    <b>Mentee: <?php echo $chat->getMentee(); ?></b>
+                                <?php } else { ?>
+                                    <b>Mentor: <?php echo $chat->getMentor(); ?></b>
+                                <?php } ?>
+                                <p>Subjects: <?php echo $postDetails->getSubject(); ?></p>
+                            </td>
+                            <td>
+                            <?php if ($postDetails->getAddress() != null) { ?>
+                                <p>Area: <?php echo $postDetails->getAddress(); ?></p>
+                            <?php } ?>
+                            </td>
+                            <td>
+                                <p><?php echo date('d M Y', $chat->getTimestamp()); ?></p>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+            <?php } else { ?>
+                <h2 class="aligncenter">You have no chat.</h2>
+    
+            <?php } ?>
+            <p style="page-break-before: always;">&nbsp;</p>
+            <p style="page-break-after: always;">&nbsp;</p>
+            <p style="page-break-before: always;">&nbsp;</p>
+            <p style="page-break-after: always;">&nbsp;</p>
+            <p style="page-break-before: always;">&nbsp;</p>
+        
+            <hr/>
+    
+        <?php } else { ?>
+            <!-- Main body of Home Page -->
+            <br>
+            <h1 class="welcome">Welcome!</h1>
+            
+            <p class="home-page-description">
+                A local/online homework help web app for disadvantaged kids who might not have any access to 
+                help around them. For example, kids who have parents that are unable to help either due to time 
+                constraint or lack of education and they may not be able to afford tutoring. 
+            </p>
+    
+            <div class="row">
+                <div class="col-sm-1"></div>
+                
+                <div class="col-sm-4">
+                    <img class="home-page-images" src="images/1.png" alt="" width="70%" height="70%">
+                    </br>
+                    <a href="register/"><button type="button" class="home-page-button button1">Seek help as a Mentee</button></a>
+                </div>
+    
+                
+                <div class="col-sm-4">
+                    <img class="home-page-images" src="images/2.png" alt="" width="65%" height="65%">
+                    </br>
+                    <a href="register/"><button type="button" class="home-page-button button2">Volunteer as a Mentor</button></a>
+                </div>
+                
+                <div class="col-sm-1"></div>
+            </div>
+    
+        <?php } ?>
+    
+        </div>
+    </article>
+
+  </body>
+</html>
